5378e1fb5502da97641cad1a71a6a6d7352e6e72
Chenjie Yang <chenjieyang@Chenjies-MacBook-Air-2.local>
2019-09-27 15:44:32 +0800

    Added standard scaler.

--- mine_estimator.py
+++ mine_estimator.py
@@ -6,7 +6,12 @@ class DistributionSimulator:
     
     def __init__(self, x, y):
         self.x = np.array(x); self.y = np.array(y)
+        self.standardize()
     
+    def standardize(self):
+        self.x = (self.x - self.x.mean())/self.x.std()
+        self.y = (self.y - self.y.mean())/self.y.std()
+
     def reshuffle(self):
         y_cpy = self.y.copy()
         pool = np.array([self.x, self.y]).T
@@ -26,24 +31,24 @@ class DistributionSimulator:
         return batch, end_idx == self.pool.shape[0]
 
 
-def mine(x, y, n_hidden=50, lr=0.1, batch_size=128, early_stopping=40):
+def mine(x, y, n_hidden=50, lr=0.05, batch_size=64, early_stopping=40):
     ds = DistributionSimulator(x, y)
-    initializer = tf.glorot_normal_initializer()
+    initializer = tf.variance_scaling_initializer(distribution='uniform')
     xy_in = tf.placeholder(tf.float32, shape=[None, 2])
     xy_bar_in = tf.placeholder(tf.float32, shape=[None, 2])
     W_1 = tf.Variable(initializer([2, n_hidden]), dtype=tf.float32)
     b_1 = tf.Variable(tf.zeros(n_hidden), dtype=tf.float32)
     z_1 = tf.matmul(xy_in, W_1) + b_1
     z_1_bar = tf.matmul(xy_bar_in, W_1) + b_1
-    a_1 = z_1*tf.nn.sigmoid(z_1)
-    a_1_bar = z_1_bar*tf.nn.sigmoid(z_1_bar)
+    a_1 = tf.nn.leaky_relu(z_1)
+    a_1_bar = tf.nn.leaky_relu(z_1_bar)
 
     W_2 = tf.Variable(initializer([n_hidden, 1]), dtype=tf.float32)
     b_2 = tf.Variable(tf.zeros(1), dtype=tf.float32)
     z_2 = tf.matmul(a_1, W_2) + b_2
     z_2_bar = tf.matmul(a_1_bar, W_2) + b_2
-    a_2 = z_2*tf.nn.sigmoid(z_2)
-    a_2_bar = z_2_bar*tf.nn.sigmoid(z_2_bar)
+    a_2 = tf.nn.leaky_relu(z_2)
+    a_2_bar = tf.nn.leaky_relu(z_2_bar)
 
     neg_loss = -(tf.reduce_mean(a_2, axis=0) - tf.math.log(tf.reduce_mean(tf.math.exp(a_2_bar), axis=0)))
     opt = tf.train.GradientDescentOptimizer(learning_rate=lr).minimize(neg_loss)
@@ -62,10 +67,11 @@ def mine(x, y, n_hidden=50, lr=0.1, batch_size=128, early_stopping=40):
             batch_x_y = np.array([batch[:, 0], batch[:, -1]]).T
             _, loss = sess.run([opt, neg_loss], feed_dict={xy_in: batch_xy, xy_bar_in: batch_x_y})
             losses.append(loss)
-        if epoch > 1.5*early_stopping:
+        if epoch > early_stopping:
             if loss >= np.max(training_loss[-early_stopping:]):
                 break
         print(f'epoch {epoch}, loss {np.mean(losses)}')
         training_loss.append(np.mean(losses))
     sess.close()
-    return -np.min(training_loss[-early_stopping:]), training_loss
+    eval_idx = max(int(early_stopping/4), 5)
+    return -np.mean(training_loss[-eval_idx:]), training_loss
